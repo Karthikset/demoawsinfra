bb999f7340b1fbdc86d635022df683de0c640c11
Karthikset <30341186+Karthikset@users.noreply.github.com>
2025-05-08 03:07:01 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region where resources will be deployed"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "my-devops-bucket"
+}
